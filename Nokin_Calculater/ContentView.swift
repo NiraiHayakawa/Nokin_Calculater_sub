e065c7ab13d26214a64bf4378e863fda45ff9e2f
NiraiHayakawa <exam.niraihayakawa@gmail.com>
2022-12-03 02:23:42 -0800

    Initial commit

---
+++ Nokin_Calculater/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Nokin_Calculater
+//
+//  Created by geo on 2022/12/03.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("DAn")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nokin_Calculater/Nokin_CalculaterApp.swift
@@ -0,0 +1,18 @@
+//
+//  Nokin_CalculaterApp.swift
+//  Nokin_Calculater
+//
+//  Created by geo on 2022/12/03.
+//
+//
+
+import SwiftUI
+
+@main
+struct Nokin_CalculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
