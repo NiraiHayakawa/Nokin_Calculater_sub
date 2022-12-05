8fab3e3fa69c8c6de26124704524e6b3c8a095f7
NiraiHayakawa <91765262+NiraiHayakawa@users.noreply.github.com>
2022-12-05 04:33:33 -0800

    aspectRatio(contentMode:. fill)

--- Nokin_Calculater/ContentView.swift
+++ Nokin_Calculater/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        // 背景に画像を全画面(ネットの受け売り)
+        ZStack {
+            //imageにはなんか好きなのを入れてね
+            Image("Image")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
             Text("Let’s look for a solution !")
+                .foregroundColor(.init(red: 255, green: 0, blue: 0))
+                .font(.title)
+                
         }
-        .padding()
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
