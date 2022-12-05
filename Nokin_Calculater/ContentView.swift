3dd14d9adc7611c5c0609f794f1159a4be69b980
Dan Amano <dan.a0607@icloud.com>
2022-12-05 23:22:54 +0900

    Update ContentView.swift

--- Nokin_Calculater/ContentView.swift
+++ Nokin_Calculater/ContentView.swift
@@ -12,16 +12,14 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Image("FormulaBlackboard")
-                .resizable()
-                .aspectRatio(contentMode:.fill)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
             Text("Let’s look for a solution !")
-                .foregroundColor(.init(red: 255, green: 0, blue: 0))
-                .font(.title)
-            
+                    .foregroundColor(.init(red: 255, green: 0, blue: 0))
+                    .font(.title)
+
         }
     }
-    let vc = self.storyboard?.instantiateViewController(withIdentifier: "toNext") as! NextViewController
-self.present(vc, animated: true)
     
     
     struct ContentView_Previews: PreviewProvider {
